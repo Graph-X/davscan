2ffc2d6e8e9d2042790e76ba62f5f31dbd73d6bb
Graph-X <nicholas_lehman@hotmail.com>
2016-08-05 18:53:48 -0500

    adding msf and dos exclusion capabilities

--- sploits.py
+++ sploits.py
@@ -8,15 +8,15 @@
 
 import subprocess
 import re
-def cleanup(self,s):
-	for k in s.keys():
-		if (k == '' or k == 'Server' or k == 'Exploit Title'):
-			del s[k]
-	return s
 	
 class IIS5:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'IIS 5.0']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.append('--no-msf')
+		command.append(['-t', 'IIS 5.0'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'IIS 5.0'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -26,8 +26,13 @@ class IIS5:
                     if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                         self.sploits[kv[0].strip()] = kv[1].strip()
 class IIS6:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'IIS 6.0']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'IIS 6.0'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'IIS 6.0'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -38,8 +43,13 @@ class IIS6:
                     self.sploits[kv[0].strip()] = kv[1].strip()
 
 class IIS75:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'IIS 7.5']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'IIS 7.5'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'IIS 7.5'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -50,8 +60,13 @@ class IIS75:
                     self.sploits[kv[0].strip()] = kv[1].strip()
 
 class Apache13:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'Apache 1.3']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'Apache 1.3'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'Apache 1.3'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -61,8 +76,13 @@ class Apache13:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                         self.sploits[kv[0].strip()] = kv[1].strip()
 class Apache20:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'Apache 2.0']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'Apache 2.0'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'Apache 2.0'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -72,8 +92,13 @@ class Apache20:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                         self.sploits[kv[0].strip()] = kv[1].strip()
 class Apache22:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'Apache 2.2']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'Apache 2.2'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'Apache 2.2'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -83,8 +108,13 @@ class Apache22:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                     self.sploits[kv[0].strip()] = kv[1].strip()
 class Apache24:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'Apache 2.4']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'Apache 2.4'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'Apache 2.4'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -94,8 +124,13 @@ class Apache24:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                     self.sploits[kv[0].strip()] = kv[1].strip()
 class nginx06:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'nginx 0.6']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'nginx 0.6'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'nginx 0.6'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -105,8 +140,13 @@ class nginx06:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                     self.sploits[kv[0].strip()] = kv[1].strip()
 class nginx078:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'nginx 0.7']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'nginx 0.7'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'nginx 0.7 or nginx 0.8'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -116,8 +156,13 @@ class nginx078:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                     self.sploits[kv[0].strip()] = kv[1].strip()
 class nginx11:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'nginx 1.1.7']
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'nginx 1.1.7'])
+		if d:
+			command.append('| grep -v "/dos/"')
         self.sploits = {'Server': 'nginx 1.1.7'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
@@ -127,9 +172,14 @@ class nginx11:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                     self.sploits[kv[0].strip()] = kv[1].strip()
 class nginx134:
-    def __init__(self):
-        command = ['searchsploit', '-t', 'nginx 1.3.9']
-        self.sploits = {'Server': 'nginx 1.3.9'}
+    def __init__(self, m=False, d=False):
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-t', 'nginx 1.3.4'])
+		if d:
+			command.append('| grep -v "/dos/"')
+        self.sploits = {'Server': 'nginx 1.3.4'}
         results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
         out = results.communicate()
         for line in out[0].splitlines():
@@ -138,26 +188,36 @@ class nginx134:
                 if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
                     self.sploits[kv[0].strip()] = kv[1].strip()
 class PHP:
-    def __init__(self, v):
-            p = 'PHP ' + v
-            command = ['searchsploit', '-e', p]
-            self.sploits = {}
-            results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
-            out = results.communicate()
-            for line in out[0].splitlines():
-                if "|" in line:
-                    kv = line.split("|")
-		    if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
-		        self.sploits[kv[0].strip()] = kv[1].strip()
+    def __init__(self, v, m=False, d=False):
+		p = 'PHP ' + v
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-e', p])
+		if d:
+			command.append('| grep -v "/dos/"')
+		self.sploits = {}
+		results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
+		out = results.communicate()
+		for line in out[0].splitlines():
+			if "|" in line:
+				kv = line.split("|")
+		if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
+			self.sploits[kv[0].strip()] = kv[1].strip()
 class ASP:
-    def __init__(self, v):
-            a = 'ASP.NET ' + v
-            command = ['searchsploit', '-e', a]
-            self.sploits = {}
-            results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
-            out = results.communicate()
-            for line in out[0].splitlines():
-                if "|" in line:
-                    kv = line.split("|")
-		    if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
-		        self.sploits[kv[0].strip()] = kv[1].strip()
+    def __init__(self, v, m=False, d=False):
+        a = 'ASP.NET ' + v
+		command = ['searchsploit']
+		if m:
+			command.appeand('--no-msf')
+		command.append(['-e', a])
+		if d:
+			command.append('| grep -v "/dos/"')
+		self.sploits = {}
+		results = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, universal_newlines=True)
+		out = results.communicate()
+		for line in out[0].splitlines():
+			if "|" in line:
+				kv = line.split("|")
+		if ( kv[0].strip()  != '' or kv[0].strip() != 'Server' or kv[0].strip() != 'Exploit Title'):
+			self.sploits[kv[0].strip()] = kv[1].strip()
